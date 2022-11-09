6236d338b85d05c95036c5e8165f94f396b435d4
Vyacheslav Smolianiuk <sana22_svv@student.ztu.edu.ua>
2022-11-09 11:18:41 +0200

    change 4-5 line messages

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,5 +2,5 @@
 Console.WriteLine("Змінити текст повідомлення на довільне у перших 3-х рядках");
 Console.WriteLine("Змінити текст повідомлення на довільне у перших 3-х рядках");
 Console.WriteLine("Змінити текст повідомлення на довільне у перших 3-х рядках");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Змінити текст повідомлення на довільне в останніх 2-х рядках програмного коду.\r\n");
+Console.WriteLine("Змінити текст повідомлення на довільне в останніх 2-х рядках програмного коду.\r\n");
