02365e62c2766dcae2e97dace8273ca4530de4b3
Vyacheslav Smolianiuk <sana22_svv@student.ztu.edu.ua>
2022-11-09 11:17:56 +0200

    change messages 1-3

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Змінити текст повідомлення на довільне у перших 3-х рядках");
+Console.WriteLine("Змінити текст повідомлення на довільне у перших 3-х рядках");
+Console.WriteLine("Змінити текст повідомлення на довільне у перших 3-х рядках");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
